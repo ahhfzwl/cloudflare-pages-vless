26507872d3e4bbaaf910156e9ae33b9edaf9862a
ahhfzwl <ahhfzwl@qq.com>
2023-12-27 16:18:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '11112222-3333-4444-aaaa-bbbbccccdddd';
 
-let proxyIP = 'cdn.xn--b6gac.eu.org';
+let proxyIP = '[2a01:4f8:c2c:123f:64:5:6810:1]';
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
